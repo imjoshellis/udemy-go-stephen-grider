84102afc7232ed3df81a80e7ca5f06fe7e8ba4c6
Josh Ellis <josh@imjoshellis.com>
2020-11-26 15:50:17 -0600

    Create deck_test.go

---
+++ deck_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+	expectedLen := 16
+	if len(d) != expectedLen {
+		t.Errorf("Expected deck length of " + fmt.Sprint(expectedLen) + " but got %v", len(d))
+	}
+
+	if d[0] != "Ace of Hearts" {
+		t.Errorf("Expected Ace of Spades but got %v", d[0])
+	}
+
+	if d[15] != "Four of Clubs" {
+		t.Errorf("Expected Four of Clubs but got %v", d[15])
+	}
+}
