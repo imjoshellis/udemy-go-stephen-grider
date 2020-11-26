bd92d9b893f8b33275d256b123f89b8e79fa4a69
Josh Ellis <josh@imjoshellis.com>
2020-11-26 14:45:40 -0600

    Progress in course

---
+++ src/deck.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Hearts", "Spades", "Diamonds", "Clubs"}
+	cardRanks := []string{"Ace", "Two", "Three", "Four"}
+	
+	for _, suit := range cardSuits {
+		for _, rank := range cardRanks {
+			cards = append(cards, rank + " of " + suit)
+		}
+	}
+	
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
